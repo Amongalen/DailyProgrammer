d13a527dd20506f17f53aef983880ae354e08e38
Adam Parys <aparys@ekokal.com>
2018-02-14 13:41:47 +0100

    First commit

---
+++ src/dailyprogrammer/DailyProgrammer.java
@@ -0,0 +1,16 @@
+package dailyprogrammer;
+
+/**
+ *
+ * @author Adam Parys
+ */
+public class DailyProgrammer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
